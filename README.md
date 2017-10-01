0e7625ba0d5a0dae66e83062e0bd3d8e71b42086
Will Smith <wsmith@linode.com>
2017-10-01 09:35:35 -0400

    Added numeric comparisons and flags
    Also added a README and did some housekeeping

---
+++ README.md
@@ -0,0 +1,23 @@
+# gatherer.go
+
+A rewrite of [gatherer_cli](https://github.com/Dorthu/gatherer_cli) in gloang
+
+### Why?
+
+This is my first project using Go, I wanted to do something easy.
+
+### How to Use
+
+```bash
+go get github.com/dorthu/gatherer_go
+go install github.com/dorthu/gatherer_go
+gatherer_go --help
+```
+
+This assumes `~/go/bin` is on your `PATH`.
+
+### Incomplete Features
+
+Right now without a `-b` this only outputs the search URL - in the future this
+will query the page itself and scrape the results into something you can see in
+your terminal (probably piped to less).
--- main.go
+++ main.go
@@ -3,22 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"reflect"
+	"runtime"
 	"strings"
 
 	"github.com/jessevdk/go-flags"
 )
 
 func main() {
-	args := parse_args()
+	args := parseArgs()
 	url := "http://gatherer.wizards.com/Pages/Search/Default.aspx?action=advanced"
 
 	full_result := fmt.Sprintf("%v%v", url, args)
 
 	fmt.Println(full_result)
+
+	if args.InBrowser {
+		openUrl(full_result)
+	}
 }
 
-func parse_args() QueryStruct {
+func parseArgs() QueryStruct {
 	var pargs QueryStruct
 
 	_, err := flags.ParseArgs(&pargs, os.Args)
@@ -28,6 +34,22 @@ func parse_args() QueryStruct {
 	}
 
 	return pargs
+}
+
+func openUrl(url string) {
+	var args []string
+
+	switch runtime.GOOS {
+	case "darwin":
+		args = []string{"open"}
+	case "windwos":
+		args = []string{"cmd", "/c", "start"}
+	default:
+		args = []string{"sensible-browser"}
+	}
+
+	cmd := exec.Command(args[0], append(args[1:], url)...)
+	cmd.Start()
 }
 
 type Color int
@@ -76,31 +98,51 @@ func ToColor(s string) Color {
 
 func getJoin(query string, default_join string) (string, string) {
 	ret := default_join
+	q := query
 	switch c := query[0]; c {
 	case '!':
 		ret = "+!"
-		query = query[1:]
+		q = query[1:]
 		break
 	case '+':
 		ret = "+"
-		query = query[1:]
+		q = query[1:]
 		break
 	case '|':
 		ret = "|"
-		query = query[1:]
+		q = query[1:]
 		break
 	}
-	return query, ret
+	return q, ret
+}
+
+func getComparison(query string, default_comparison string) (string, string) {
+	ret := default_comparison
+	q := query
+	switch c := query[0]; c {
+	case '=':
+		fallthrough
+	case '<':
+		fallthrough
+	case '>':
+		ret = query[:1]
+		q = query[1:]
+	}
+
+	return q, ret
 }
 
 type QueryStruct struct {
 	Type              string `short:"t" description:"The card type" join:"+" split:" " query:"type"`
 	Suptype           string `long:"st" description:":The card's subtype" join:"+" split:" " query:"subtype"`
 	Name              string `short:"n" description:"The card name" join:"+" split:" " query:"name"`
-	ConvertedManaCost string `long:"cmc" description:"The card's converted mana cast" join:"+=" query:"cmc"`
+	ConvertedManaCost string `long:"cmc" description:"The card's converted mana cast" join:"+=" query:"cmc" comparison:"="`
 	Color             string `short:"c" description:"The card's color" join:"+" split:"" query:"color" converter:"color"`
 	ColorIdentiy      string `short:"i" description:"The card's color identity" join:"+" split:"" query:"coloridentiy" converter:"color"`
 	Rules             string `short:"r" description:"The card's rules test" join:"+" split:" " query:"text"`
+	InBrowser         bool   `short:"b" description:"Open the result in the default web browser" skip:"yes"`
+	Power             string `long:"pow" description:"The creature's power" query:"power" comparison:"="`
+	Toughness         string `long:"tough" description:"The creature's toughness" query:"power" comparison:"="`
 }
 
 func (query QueryStruct) String() string {
@@ -109,26 +151,50 @@ func (query QueryStruct) String() string {
 
 	result := ""
 
+	/// loop over the arguments
 	for i := 0; i < val.NumField(); i++ {
 		key := val.Type().Field(i)
 		val := val.Field(i)
 
+		/// if it wasn't present, move on
 		if val.String() == "" {
 			continue
 		}
 
-		current := fmt.Sprintf("&%v=", key.Tag.Get("query"))
-
-		parts := strings.Split(val.String(), key.Tag.Get("split"))
-
-		join := key.Tag.Get("join")
-
-		parts[0], join = getJoin(parts[0], join)
-
-		for _, c := range parts {
-			current = fmt.Sprintf("%v%v[%v]", current, join, c)
+		/// if it's supposed to be skipped, move on
+		if key.Tag.Get("skip") != "" {
+			continue
 		}
 
+		/// set up the query string
+		current := fmt.Sprintf("&%v=", key.Tag.Get("query"))
+
+		/// if we're looking at a string field
+		if key.Tag.Get("split") != "" {
+			/// perform the split and get the join
+			parts := strings.Split(val.String(), key.Tag.Get("split"))
+
+			join := key.Tag.Get("join")
+
+			parts[0], join = getJoin(parts[0], join)
+
+			/// clear out the join if it was given
+			if parts[0] == "" {
+				parts = parts[1:]
+			}
+
+			/// join the parts and format
+			for _, c := range parts {
+				current = fmt.Sprintf("%v%v[%v]", current, join, c)
+			}
+		} else {
+			/// this is a numeric
+			number, comparison := getComparison(val.String(), key.Tag.Get("comparison"))
+
+			current = fmt.Sprintf("%v+%v[%v]", current, comparison, number)
+		}
+
+		/// append it to the result
 		result = fmt.Sprintf("%v%v", result, current)
 	}
 
